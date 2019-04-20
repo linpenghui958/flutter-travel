1be132252b3a168e5784c68582b85d0956946b22
melonlin <melonlin@tencent.com>
2019-04-20 16:17:18 +0800

    add opacity appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,35 +6,87 @@ class HomePage extends StatefulWidget{
   _HomePageState createState() => _HomePageState();
 }
 
+const APPBAR_SCROLL_OFFSET = 100;
+
 class _HomePageState extends State<HomePage> {
   List _imageUrls = [
-    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556337683&di=4c27e7f1ec33c9218bb87594e546b33c&imgtype=jpg&er=1&src=http%3A%2F%2Fpic15.nipic.com%2F20110628%2F1369025_192645024000_2.jpg',
     'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
     'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
   ];
+
+  double appBarAplh = 0;
+
+  _onScroll(offset) {
+    double aplh = offset / APPBAR_SCROLL_OFFSET;
+    if (aplh < 0) {
+      aplh = 0;
+    } else if (aplh > 1) {
+      aplh = 1;
+    }
+    setState(() {
+      appBarAplh = aplh;
+    });
+    print(appBarAplh);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 160,
-              child: Swiper(
-                itemCount: _imageUrls.length,
-                autoplay: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _imageUrls[index],
-                    fit: BoxFit.fill,
-                  );
-                },
-                pagination: SwiperPagination(),
-              )
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if(scrollNotification is ScrollUpdateNotification && 
+                  scrollNotification.depth == 0) {
+                    // 检测滚动，并且是listView滚动
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    )
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('linph'),
+                    ),
+                  )
+                ],
+              ),
             )
-          ],
-        )
-      ),
+          ),
+          Opacity(
+            opacity: appBarAplh,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('home page'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ) 
     );
   }
 }
